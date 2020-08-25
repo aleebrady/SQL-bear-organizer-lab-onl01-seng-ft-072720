fce0146ad531140d43b5448a7f367703f704ada6
Aaron Brady <aleebrady@gmail.com>
2020-08-25 17:25:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "Male", "Brown", "Yogi-Like", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy" 10, "Male", "Brown", "Yogi-Like", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha" 10, "Male", "Brown", "Yogi-Like", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown" 10, "Male", "Brown", "Yogi-Like", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa" 10, "Male", "Brown", "Yogi-Like", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch" 10, "Male", "Brown", "Yogi-Like", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy" 10, "Male", "Brown", "Yogi-Like", 0);
-Insert Into bears (name, age, gender, color, temperament, alive) VALUES (NULL 10, "Male", "Brown", "Aggressive", 0);

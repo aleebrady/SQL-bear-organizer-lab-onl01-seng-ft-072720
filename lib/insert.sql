e8d995a2c0a2c5674490073bee269ac503c6faf7
Aaron Brady <aleebrady@gmail.com>
2020-08-25 17:20:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ Insert Into bears (name, age, gender, color, temperament, alive) Values ("Mr. Ch
 Insert Into bears (name, age, gender, color, temperament, alive) Values ("Rowdy" 10, "Male", "Brown", "Yogi-Like", 0);
 Insert Into bears (name, age, gender, color, temperament, alive) Values ("Tabitha" 10, "Male", "Brown", "Yogi-Like", 0);
 Insert Into bears (name, age, gender, color, temperament, alive) Values ("Sergeant Brown" 10, "Male", "Brown", "Yogi-Like", 0);
-Insert Into bears (name, age, gender, color, temperament, alive) Values ("Melissa");
-Insert Into bears (name, age, gender, color, temperament, alive) Values ("Grinch");
-Insert Into bears (name, age, gender, color, temperament, alive) Values ("Wendy");
-Insert Into bears (name, age, gender, color, temperament, alive) Values (NULL);
+Insert Into bears (name, age, gender, color, temperament, alive) Values ("Melissa" 10, "Male", "Brown", "Yogi-Like", 0);
+Insert Into bears (name, age, gender, color, temperament, alive) Values ("Grinch" 10, "Male", "Brown", "Yogi-Like", 0);
+Insert Into bears (name, age, gender, color, temperament, alive) Values ("Wendy" 10, "Male", "Brown", "Yogi-Like", 0);
+Insert Into bears (name, age, gender, color, temperament, alive) Values (NULL 10, "Male", "Brown", "Aggressive", 0);

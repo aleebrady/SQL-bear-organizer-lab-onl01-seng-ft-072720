b94a95376c98642489a2ecd87a430abda3282833
Aaron Brady <aleebrady@gmail.com>
2020-08-25 17:28:11 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
 Insert Into bears (name, age, gender, color, temperament, alive) 
-Values ("Mr. Chocolate", 10, "Male", "Brown", "Yogi-Like", 0); 
+Values ("Mr. Chocolate", 10, "Male", "Brown", "Yogi-Like", 0); ("Rowdy" 10, "Male", "Brown", "Yogi-Like", 0);

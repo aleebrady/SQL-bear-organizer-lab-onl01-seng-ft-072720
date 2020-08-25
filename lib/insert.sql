7f2d09c7622d249850212fbd4886ef94aba4848d
Aaron Brady <aleebrady@gmail.com>
2020-08-25 17:13:30 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ Insert Into bears (name, age, gender, color, temperament, alive) Values ("Rowdy"
 Insert Into bears (name, age, gender, color, temperament, alive) Values ("Tabitha");
 Insert Into bears (name, age, gender, color, temperament, alive) Values ("Sergeant Brown");
 Insert Into bears (name, age, gender, color, temperament, alive) Values ("Melissa");
-Insert Into bears (name, age, gender, color, temperament, alive) Values ("Mr. Chocolate");
+Insert Into bears (name, age, gender, color, temperament, alive) Values ("Grinch");
 Insert Into bears (name, age, gender, color, temperament, alive) Values ("Mr. Chocolate");
 Insert Into bears (name, age, gender, color, temperament, alive) Values ("Mr. Chocolate");
